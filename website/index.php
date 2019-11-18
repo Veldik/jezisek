6db211185aef766974d816597532f55bf8f44203
Velda <gamervelda@gmail.com>
2019-11-18 21:37:57 +0100

    Upload webu

---
+++ website/index.php
@@ -0,0 +1,60 @@
+<html>
+    <head>
+
+        <title>Ježíšek</title>
+        <meta name="theme-color" content="#000000">
+        <meta name="description" content="Jsem Vánoční Discord bot, který přehrává Vánoční písničky.">
+        <meta property="og:image" content="images/logo.png">
+        <link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
+        <style>
+            html{
+                width: 100%;
+                height: 100%;
+            }
+            body{
+                background-color: #000000;
+                color: #ffffff;
+                font-family: sans-serif;
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+            }
+            div{
+                position: absolute;
+                transform: translate(-50%, -50%);
+                left: 50%;
+                top: 50%;
+            }
+            h1{
+                color: #ffffff;
+                font-size: 120px;
+                text-align: center;
+                letter-spacing: -5px;
+            }
+            p{
+                color: #242424;
+                text-align: center;
+            }
+            h2{
+                display: block;
+                color: #ffffff;
+                text-align: center;
+                text-transform: uppercase;
+                padding-top: -70px;
+            }
+        </style>
+    <head>
+    <body>
+        <div>
+            <h1>
+                Ježíšek
+            </h1>
+
+            <h2>
+                Jsem Vánoční Discord bot, který přehrává Vánoční písničky.<br>
+                <a href="https://discordapp.com/oauth2/authorize?client_id=642795374150418438&scope=bot" style="text-decoration: none;" ><span style="color: #32d8ff">Přidej si mě k sobě!</span></a><br>
+            </h2>
+        </div>
+    <body>
+<html>
