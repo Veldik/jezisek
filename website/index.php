1501741f15396dcf6c1acf35213128c6bd2409bc
Velda <gamervelda@gmail.com>
2019-11-26 20:36:13 +0100

    Drobná úprava tagů

--- website/index.php
+++ website/index.php
@@ -13,10 +13,7 @@
     <head>
     <body id="particles">
         <div>
-            <h1>
-                Ježíšek
-            </h1>
-
+            <h1>Ježíšek</h1>
             <h2>
                 <span>Jsem Vánoční Discord bot, který přehrává Vánoční písničky.</span><br>
                 <a href="https://discordapp.com/oauth2/authorize?client_id=642795374150418438&scope=bot" class="no-decoration"><span class="url">Přidej si mě k sobě!</span></a>
@@ -28,4 +25,4 @@
     <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
     <script src="https://threejs.org/examples/js/libs/stats.min.js"></script>
     <script>particlesJS("particles", {"particles":{"number":{"value":400,"density":{"enable":true,"value_area":800}},"color":{"value":"#fff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":10,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":500,"color":"#ffffff","opacity":0.4,"width":2},"move":{"enable":true,"speed":6,"direction":"bottom","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"bubble"},"onclick":{"enable":false,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":0.5}},"bubble":{"distance":400,"size":4,"duration":0.3,"opacity":1,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});</script>
-<html>
+</html>
